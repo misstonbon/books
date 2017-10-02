f448b5fbb66ce23992ede5ccfc67043de9847e49
Tanja Stroble <plusdeltah@gmail.com>
2017-10-01 18:17:45 -0700

    added categories but nav bar doesn't work yet

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,12 @@ class BooksController < ApplicationController
   before_action :find_book, only: [:show, :edit, :update, :destroy]
 
   def index
-    @books = Book.all.order("created_at DESC")
+    if params[:category].blank?
+      @books = Book.all.order("created_at DESC")
+    else
+      @category_id = Category.find_by(name: params[:category]).id
+      @books = Book.where(:category_id => @category_id).order("created_at DESC")
+    end 
   end
 
   def show
@@ -10,10 +15,15 @@ class BooksController < ApplicationController
 
   def new
     @book = current_user.books.build #builds books from current user
+    @categories = Category.all.map { |c| [c.name, c.id] }
+    #when we create select tag in dropdown menu in form of
+    #partial file, options_for_select requires an array of arrays
+    #which provide the text for dropdown option (name) and value (id)
   end
 
   def create
     @book = current_user.books.build(book_params)
+    @book.category_id = params[:category_id]
     if @book.save
       redirect_to root_path
     else
@@ -22,9 +32,11 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @categories = Category.all.map { |c| [c.name, c.id] }
   end
 
   def update
+      @book.category_id = params[:category_id]
     if
       @book.update(book_params)
       redirect_to book_path(@book)
@@ -41,7 +53,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :description, :author)
+    params.require(:book).permit(:title, :description, :author, :category_id)
   end
 
   def find_book
